280bcbac061b4fb5d3deeee393299387a84ee25e
Ankur Tiwary <33143095+ankurTiwxry@users.noreply.github.com>
2024-05-11 19:07:34 +0200

    some more changes

--- pn_docs_next/next.config.mjs
+++ pn_docs_next/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "/pn_shop/pn_docs_next",
   output: "export",
   images: {
     unoptimized: true,
