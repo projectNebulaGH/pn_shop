cabe449b2565437164067972137e13752b710140
Ankur Tiwary <33143095+ankurTiwxry@users.noreply.github.com>
2024-05-14 17:51:50 +0200

    feat: made some release changes here onb develop

--- pn_docs_next/next.config.mjs
+++ pn_docs_next/next.config.mjs
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
-  images: {
-    unoptimized: true,
-  },
+  basePath: "https://projectnebulagh.github.io/pn_shop/pn_docs_next",
+  'images.unoptimised' : true,
+
 };
 
 export default nextConfig;
