6207034baef56d636a6af70f84f3c898d5d57772
Ankur Tiwary <33143095+ankurTiwxry@users.noreply.github.com>
2024-05-18 15:19:20 +0200

    feat: added working modals

--- pn_shop_next/app/ProductsPage/product-frame.tsx
+++ pn_shop_next/app/ProductsPage/product-frame.tsx
@@ -1,20 +1,52 @@
 'use client'
 
+import Modal from "@/components/Modal/modal";
+import { useState } from "react";
+
 interface ProductFrameProps {
     name: string
 }
 
-export const ProductFrame = ({name}: ProductFrameProps) => {
+export const ProductFrame = ({ name }: ProductFrameProps) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
     return (
-        <button className="
-            bg-neumorphic-gradient text-light-grey px-4 py-1 mx-2 
-            min-h-[8vh] w-[12vh] rounded-neumorphic font-medium 
-            cursor-pointer shadow-neumorphic-light
-            hover:shadow-neumorphic-hover
-            active:shadow-neumorphic-active
-            transition-shadow duration-200 ease-in-out
-        ">
-            {name}
-        </button>
+        <div>
+            <button
+                className="
+                bg-neumorphic-gradient text-light-grey px-4 py-1 mx-2 
+                min-h-[8vh] w-[12vh] rounded-neumorphic font-medium 
+                cursor-pointer shadow-neumorphic-light
+                hover:shadow-neumorphic-hover
+                active:shadow-neumorphic-active
+                transition-shadow duration-200 ease-in-out
+            "
+                onClick={openModal}
+            >
+                {name}
+            </button>
+            {isModalOpen && (
+                <Modal 
+                closed={closeModal} 
+                component={<ProductModalFrame name={name} />} />
+            )}
+        </div>
+    );
+};
+
+export const ProductModalFrame = ({name}: ProductFrameProps) => {
+    return (
+        <div className="flex flex-col">
+            <p className="text-primary text-4xl pt-[2vh] pb-[2vh]">{name}</p>
+            <p className="text-light-grey text-lg font bold pt-[2vh] pb-[2vh] font-medium">{"Description"}</p>
+        </div>
     )
 }
---
+++ pn_shop_next/components/Modal/modal.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import React from "react";
+
+interface ModalProps {
+  closed: (text: boolean) => void;
+  component: React.ReactNode; // Define the prop to accept a component
+}
+
+const Modal: React.FC<ModalProps> = ({ closed, component }: ModalProps) => {
+  return (
+    <>
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="h-screen w-screen fixed" onClick={() => closed(true)}></div>
+        <div className="relative w-auto my-6 mx-auto max-w-5xl">
+          <div className="backdrop-blur-xl rounded-3xl shadow-lg relative flex flex-col w-full bg-base-color-alt/60 outline-none focus:outline-none max-h-[800px] overflow-y-scroll no-scrollbar">
+            <div
+              className="fixed right-3 top-3 hover:cursor-pointer"
+              onClick={() => {
+                closed(true);
+              }}
+            >
+              <p className="text-light-grey text-4xl pt-[2vh] pb-[2vh] font-bold">{"X"}</p>
+            </div>
+            <div className="min-w-[40vw] min-h-[30vh] p-[2vw]">
+              {component}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+    </>
+  );
+};
+
+export default Modal;
